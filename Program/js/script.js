9b421bc09cb7cfcd2ad8dc47f20ac5ecf3169467
TriYuliann <mayariana1206@gmail.com>
2024-01-21 00:57:45 +0700

    mengubah link baca selengkapnya pada index.html menjadi button menggunakan javascript agar terhubung ke link berita

--- Program/js/script.js
+++ Program/js/script.js
@@ -1,4 +1,12 @@
 //link pendaftaran
 document.getElementById("btn-df").addEventListener("click", function() {
-window.open("https://forms.gle/Xy9MjEXBdEJhUp3N8", "_blank");
+    window.open("https://forms.gle/Xy9MjEXBdEJhUp3N8", "_blank");
+});
+
+//link baca selengkapnya berita
+document.getElementById("btn-br1").addEventListener("click", function() {
+    window.location.href = "baca-selengkapnya.html";
+});
+document.getElementById("btn-br2").addEventListener("click", function() {
+    window.location.href = "baca-selengkapnya.html";
 });
