999a3ae67743e5993b32652736cd470ec689d0d7
TriYuliann <mayariana1206@gmail.com>
2024-01-21 01:36:34 +0700

    mengubah hyperlink menu home index.html menggunakan javascript 1/5

--- Program/js/script.js
+++ Program/js/script.js
@@ -1,12 +1,26 @@
-//link pendaftaran
-document.getElementById("btn-df").addEventListener("click", function() {
-    window.open("https://forms.gle/Xy9MjEXBdEJhUp3N8", "_blank");
+//link Home
+document.getElementById("home").addEventListener("click", function() {
+    window.location.href = "index.html";
 });
 
+
+//--------------------------------------------------------------------------------------
+//link pendaftaran
+    document.getElementById("btn-df").addEventListener("click", function() {
+        window.open("https://forms.gle/Xy9MjEXBdEJhUp3N8", "_blank");
+    });
+
 //link baca selengkapnya berita
-document.getElementById("btn-br1").addEventListener("click", function() {
-    window.location.href = "baca-selengkapnya.html";
-});
-document.getElementById("btn-br2").addEventListener("click", function() {
-    window.location.href = "baca-selengkapnya.html";
-});
+    document.getElementById("btn-br1").addEventListener("click", function() {
+        window.location.href = "baca-selengkapnya.html";
+    });
+    document.getElementById("btn-br2").addEventListener("click", function() {
+        window.location.href = "baca-selengkapnya.html";
+    });
+
+//link Social media icon halaman kontak.html
+    //Facebook
+    document.getElementById("icn-fb").addEventListener("click", function() {
+        window.open("https://www.facebook.com/farmasipunyacerita/", "_blank");
+    });
+    
