f12a80b921f5cf683a3c31d8e1cec187be38a76e
TriYuliann <mayariana1206@gmail.com>
2024-01-21 22:53:12 +0700

    memperbarui tampilan profile menggunakan javascript 4/5

--- Program/js/tes.js
+++ Program/js/tes.js
@@ -19,3 +19,10 @@ document.querySelectorAll('.wj-gallery-box').forEach((el, i) => {
       console.log('tess')
     }, (700 * (Math.exp(i * 0.30))) - 700);
   });
+
+  document.querySelectorAll('.wj-gallery-box4').forEach((el, i) => {
+    setTimeout(() => {
+      el.classList.add('wj-show-el');
+      console.log('tess')
+    }, (700 * (Math.exp(i * 0.30))) - 700);
+  });
